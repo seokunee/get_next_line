56f4447e912f8d99afa68d93cfa874a6867103bb
seokunee <xocoolree@gmail.com>
2022-04-04 23:49:40 +0900

    20220404

--- get_next_line.c
+++ get_next_line.c
@@ -6,34 +6,67 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 13:41:21 by seokchoi          #+#    #+#             */
-/*   Updated: 2022/03/31 17:13:11 by seokchoi         ###   ########.fr       */
+/*   Updated: 2022/04/04 23:49:28 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	read_one_line_defore_newline(char *buf)
+int	ft_ckeck_newline(char *str)
 {
-	
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
 }
+
+char	*ft_find_line(char *buf, ssize_t rdsize)
+{
+	ssize_t	i;
+	ssize_t	
+
+	i = 0;
+
+	while ()
+}
+
 
 char	*get_next_line(int fd)
 {
 	char	*buf[BUFFER_SIZE + 1];
-	char	*line;
+	char	*strs[OPEN_MAX];
+	char	*tmp;
 	ssize_t	rdsize;
 
 	if (BUFFER_SIZE < 1 || fd < 0 || fd > OPEN_MAX)
 		return (0);
-	buf = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (0);
 	rdsize = read(fd, buf, O_RDONLY);
 	while (rdsize > 0)
 	{
-		rdsize = read(fd, buf)
+		buf[rdsize] = '\0';
+		if (!strs[fd])
+			strs[fd] = ft_strdup("");
+		if (!strs[fd])
+			return (0);
+		tmp = strs[fd];
+		strs[fd] = ft_strjoin(tmp, buf);
+		free(tmp);
+		if (!str[fd])
+			return (0);
+		if (ft_ckeck_newline(strs[fd]) >= 0)
+			break;
+		rdsize = read(fd, buf, BUFFER_SIZE);
 	}
-	return ();
+	return (ft_find_line(&line[fd], rdsize));
 }
 
 int	main(void)
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,19 @@
 /*   By: seokchoi <seokchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 13:41:12 by seokchoi          #+#    #+#             */
-/*   Updated: 2022/03/31 16:14:09 by seokchoi         ###   ########.fr       */
+/*   Updated: 2022/04/04 20:27:04 by seokchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define  BUFFER_SIZE=42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# define TRUE 1
+# define FALSE 0
 # include <stdlib.h>
 # include <unistd.h>
 # include <limits.h>
